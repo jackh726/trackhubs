2ac4d2e44b0d08dd5fa086ef9a78085bbc1623f7
kitskub <jack.huey@umassmed.edu>
2017-08-19 15:05:57 -0400

    Basic form structure

---
+++ src/js/data.js
@@ -0,0 +1,18 @@
+class Track {
+    //track_name;
+    //track_data_URL;
+    //short_label;
+    //long_label;
+    //track_type;
+
+    constructor() {}
+}
+
+class Genome {
+    //name
+    //tracks
+
+    constructor(name) {
+        this.name = name;
+    }    
+}
