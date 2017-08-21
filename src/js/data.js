f0da21775a36591e4644503af320eee0bb7cafd7
jackh726 <jack.huey@umassmed.edu>
2017-08-21 17:16:32 -0400

    Now export button downloads a zip file with complete structure.

--- src/js/data.js
+++ src/js/data.js
@@ -23,3 +23,46 @@ class Genome {
         this.tracks = {};
     }
 }
+
+function getHubExportData(hubinfo) {
+    let out = "";
+    out += "hub " + hubinfo["hub"] + "\n";
+    for (key in hubinfo) {
+        if (key == "hub") continue;
+        out += key + " " + hubinfo[key] + "\n";
+    }
+    return out;
+}
+
+function getGenomesExportData(genomes) {
+    let out = "";
+    let first = true;
+    for (key in genomes) {
+        if (first) {
+            first = false;
+        } else {
+            out += "\n";
+        }
+        out += "genome " + key + "\n";
+        out += "trackDb " + key + "\\trackDb.txt\n";
+    }
+    return out;
+}
+
+function getTrackDBExportData(genome) {
+    let tracks = genome["tracks"];
+    let out = "";
+    let first = true;
+    for (t in tracks) {
+        let track = tracks[t];
+        if (first) {
+            first = false;
+        } else {
+            out += "\n";
+        }
+        for (key in track) {
+            out += key + " " + track[key] + "\n";
+        }
+    }
+    return out;
+}
